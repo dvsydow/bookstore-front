196d4167cfea92bdbfe61f5daa5b2f12be423fba
dvsyd <dvsyd@NITRO5-DVS>
2021-09-10 22:12:01 -0300

    acentos

--- bookstore-front/src/app/components/view/livro/livro-create/livro-create.component.ts
+++ bookstore-front/src/app/components/view/livro/livro-create/livro-create.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { LivroService } from '../livro.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Livro } from '../livro.model';
 
 @Component({
   selector: 'app-livro-create',
@@ -8,14 +11,37 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class LivroCreateComponent implements OnInit {
 
+  id_cat: String = '';
+  livro: Livro = {
+    id: '',
+    titulo: '',
+    nome_autor: '',
+    texto: ''
+  }
   titulo = new FormControl('', [Validators.minLength(3)]);
   // tslint:disable-next-line: variable-name
   nome_autor = new FormControl('', [Validators.minLength(3)]);
   texto = new FormControl('', [Validators.minLength(10)]);
 
-  constructor() { }
+  constructor(private service: LivroService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  create(): void {
+    this.id_cat = this.route.snapshot.paramMap.get('id_cat');
+    this.service.create(this.livro, this.id_cat).subscribe((resposta) => {
+      this.router.navigate([`categorias/${this.id_cat}/livros`]);
+      this.service.mensagem('Livro Criado com sucesso!');
+    }, err => {
+      this.router.navigate([`categorias/${this.id_cat}/livros`]);
+      this.service.mensagem('Erro ao criar novo Livro! Tente mais tarde!');
+    })
+  }
+
+  cancelar(): void {
+      this.id_cat = this.route.snapshot.paramMap.get('id_cat');
+      this.router.navigate([`categorias/${this.id_cat}/livros`]);
   }
 
   // tslint:disable-next-line: typedef
